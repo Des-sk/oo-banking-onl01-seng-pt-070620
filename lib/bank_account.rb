bf625c38df658436bb12585361390e311b458906
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-05 00:31:00 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,30 +1,27 @@
 class BankAccount
-  
-attr_accessor :balance, :status
-attr_reader :name
+  attr_reader :name
+  attr_accessor :balance, :status
 
-def initialize(name)
-  @name=name
-  @balance= 1000
-  @status="open"
-  
-end
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
 
-def deposit(amount)
-    self.balance += amount
-end
-  
-def display_balance
-    "Your balance is $#{self.balance}."
-end
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
 
-def valid?
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def valid?
     balance > 0 && status == "open"
-end
-
-def close_account
-  self.status = "closed"
-end
-
+  end
 
 end
