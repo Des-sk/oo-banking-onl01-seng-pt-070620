f344474d5f655a598d543a266a152024572a913d
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-05 00:32:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,8 +25,7 @@ class Transfer
 
   def reverse_transfer
     if valid? && receiver.balance > amount && self.status == "complete"
-      receiver.balance -= amount
-      sender.balance += amount
+      receiver.balance -= amount && sender.balance += amount
       self.status = "reversed"
     else
       reject_transfer
